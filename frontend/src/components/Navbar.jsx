5834d482cff65f352dc7565745c764d810b5f0a3
sahaniaditya <adityasahani893@gmail.com>
2024-09-15 20:49:35 +0530

    bug fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         {/* Logout Button */}
         <button
           className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-red-600 transition duration-300"
-          onClick={() => (window.location.href = "/login")}
+          onClick={() => (window.location.href = "/")}
         >
           Logout
         </button>
